b59d95f5aa7a803127f209c51d692b7106c90d8f
Frank <fcarter@wpsmd.org>
2019-09-26 12:01:21 -0400

    Create Pig Latin.py

---
+++ Pig Latin.py
@@ -0,0 +1,10 @@
+myWord = input("Choose a word")
+myList = ["a","e","i","o","u"]
+if (myWord[0:1] in myList):
+    print(myWord + "yay")
+else:
+    print(myWord[1:] + myWord[0:1] +"ay")
+
+
+
+    
